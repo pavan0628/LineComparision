588850d1ba6883958f27f34e846112219f4e30b9
pavan0628 <pavan.cse.rymec@gmail.com>
2025-04-11 22:22:11 +0530

    Added: comparing two length

--- src/com/bl/oops/LineComparision.java
+++ src/com/bl/oops/LineComparision.java
@@ -9,7 +9,7 @@ public class LineComparision {
 		System.out.println("Hii Welcome to LineComparision program");
 		Scanner sc = new Scanner(System.in);
 		
-		//taking user input to find the length
+		//taking user input to find the length 1
 		System.out.println("Enter x1 coordinate : ");
 		int x1 = sc.nextInt();
 		System.out.println("Enter x2 coordinate: ");
@@ -18,8 +18,24 @@ public class LineComparision {
 		int y1 = sc.nextInt();
 		System.out.println("Enter y2 coordinate : ");
 		int y2 = sc.nextInt();
-		double lengthOfaLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		System.out.println("The length of a line : " + lengthOfaLine);
-		sc.close();
+		
+		 // taking the user input for length 2
+        System.out.println("Enter a1 coordinate: ");
+        int a1 = sc.nextInt();
+        System.out.println("Enter a2 coordinate: ");
+        int a2 = sc.nextInt();
+        System.out.println("Enter b1 coordinate: ");
+        int b1 = sc.nextInt();
+        System.out.println("Enter b2 coordinate: ");
+        int b2 = sc.nextInt();
+
+        double lengthOfaLine1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double lengthOfaLine2 = Math.sqrt(Math.pow((a2 - a1), 2) + Math.pow((b2 - b1), 2));
+        System.out.println("The length of a line : " + lengthOfaLine1);
+        if (lengthOfaLine1 == lengthOfaLine2) {
+            System.out.println("The given to lengths are equal");
+        } else
+            System.out.println("Not equal");
+        sc.close();
 	}
 }
